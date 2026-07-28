d3734d796b942f8788e0f313607a4822c140878b
dsnakam <dsnakam@gmail.com>
2026-07-28 14:33:08 -0300

    Initial commit

---
+++ src/main/java/dsnakam/escolas_perto_de_mim/EscolasPertoDeMimApplication.java
@@ -0,0 +1,13 @@
+package dsnakam.escolas_perto_de_mim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolasPertoDeMimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolasPertoDeMimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dsnakam/escolas_perto_de_mim/EscolasPertoDeMimApplicationTests.java
@@ -0,0 +1,13 @@
+package dsnakam.escolas_perto_de_mim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolasPertoDeMimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
