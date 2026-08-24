9122ca831697d22f38c6a04fa43ee50dd23aabb4
dsnakam <dsnakam@gmail.com>
2026-08-24 18:33:23 -0300

    feat: adicionar rota GET /escolas-proximas

---
+++ src/main/java/dsnakam/escolas_perto_de_mim/controller/BuscaEscolaController.java
@@ -0,0 +1,31 @@
+package dsnakam.escolas_perto_de_mim.controller;
+
+import dsnakam.escolas_perto_de_mim.dto.GeocodigoUsuarioDTO;
+import dsnakam.escolas_perto_de_mim.service.DistanciaService;
+import dsnakam.escolas_perto_de_mim.service.GeocodigoUsuarioService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BuscaEscolaController {
+    private final GeocodigoUsuarioService geocodigoUsuarioService;
+    private final DistanciaService distanciaService;
+
+    public BuscaEscolaController(GeocodigoUsuarioService geocodigoUsuarioService, DistanciaService distanciaService) {
+        this.geocodigoUsuarioService = geocodigoUsuarioService;
+        this.distanciaService = distanciaService;
+    }
+
+    @GetMapping("/escolas-proximas")
+    public List<DistanciaService.EscolaDistancia> buscarEscolas(@RequestParam("q") String q) {
+        GeocodigoUsuarioDTO geocodigoUsuario = geocodigoUsuarioService.findCoordinates(q).get(0);
+        double latUsuario = Double.parseDouble(geocodigoUsuario.getLat());
+        double lonUsuario = Double.parseDouble(geocodigoUsuario.getLon());
+
+        return distanciaService.searchEscolasProximas(latUsuario, lonUsuario);
+    }
+
+}
